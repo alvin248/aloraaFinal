059ab0c84fd4d3fce4361484d87eeb4c62636e36
alvin248 <49872756+alvin248@users.noreply.github.com>
2019-08-15 11:09:15 -0400

    Add files via upload

---
+++ assets/data/bookData.ts
@@ -0,0 +1,7 @@
+import { Book } from '../../app/book';
+
+export const MyBooks : Book[] = [
+    { name: 'book1', publisher: 'pub1', author:'Alvin'},
+    { name: 'book2', publisher: 'pub2', author:'Rob'},
+    { name: 'book3', publisher: 'pub3', author:'Sarah'}
+];
